a6e1b0b7df3262f986dc30deec6314ae84e5f46a
amacapy <amankp1701@gmail.com>
2023-09-08 16:51:44 +0530

    trying to update train_step

--- main.py
+++ main.py
@@ -19,21 +19,22 @@ class NormalScalar(layers.Layer):
         pass
 
     def forward(self, x):
-        return x * 0.2 * tf.random.normal(1, 0, 1)
+        return x * tf.random.normal(1, 0, 1)
 
 
-# adapted from https://keras.io/examples/generative/vae/
 class VAE(models.Model):
     def __init__(self, latent_size):
-        super().__init__()
+        super().__init__(latent_size)
         self.total_loss_tracker = metrics.Mean("total_loss")
         self.recon_loss_tracker = metrics.Mean("recon_loss")
         self.kld_loss_tracker = metrics.Mean("kld_loss")
 
+        self.latent_size = latent_size    
+        self.era = 0
+
         self.encoder = self.get_encoder()
         self.decoder = self.get_decoder()
 
-        self.latent_size = latent_size    
 
     def get_built_shadow(self):
         shadow = Model(inputs=self.encoder.inputs, outputs=self.decoder(self.encoder.outputs[2]))
@@ -47,6 +48,7 @@ class VAE(models.Model):
         x = layers.Conv2D(64, 4, 2, activation="relu")(x)
         x = layers.Conv2D(128, 4, 2, activation="relu")(x)
         x = layers.Conv2D(256, 4, 2, activation="relu")(x)
+        x = x + layers.MultiHeadAttention(8, 128)(x, x)
         x = layers.Flatten()(x)
 
         mu = layers.Dense(self.latent_size)(x)
@@ -56,15 +58,15 @@ class VAE(models.Model):
         return Model(inputs=inputs, outputs=[mu, logs2, z])
     
     def get_decoder(self):
-        inputs = layers.Input((self.latent_size, ))
+        inputs = layers.Input((self.latent_size,))
 
-        x = layers.Dense(128, activation="relu")(inputs)
-        x = layers.Reshape((1, 1, self.latent_size))(x)
-        x = layers.Conv2DTranspose(256, 5, 2, activation="relu")(x)
+        x = layers.Reshape((1, 1, self.latent_size))(inputs)
+        x = layers.Dense(self.latent_size, activation="relu")(x)
         x = layers.Conv2DTranspose(128, 5, 2, activation="relu")(x)
-        x = layers.Conv2DTranspose(64, 6, 2, activation="relu")(x)
+        x = layers.Conv2DTranspose(64, 5, 2, activation="relu")(x)
+        x = layers.Conv2DTranspose(32, 6, 2, activation="relu")(x)
 
-        outputs = layers.Conv2DTranspose(3, 6, 2, activation="tanh")(x)
+        outputs = layers.Conv2DTranspose(3, 6, 2, activation="relu")(x)
 
         return Model(inputs=inputs, outputs=outputs)
     
@@ -72,19 +74,16 @@ class VAE(models.Model):
     def metrics(self):
         return [self.total_loss_tracker, self.recon_loss_tracker, self.kld_loss_tracker]
     
-    def __call__(self, x):
-        _, _, z= self.encoder(x)
-        return self.decoder(z)
-    
     def train_step(self, data):
         with tf.GradientTape() as tape:
             mu, logs2, z = self.encoder(data)
             recon = self.decoder(z)
 
-            recon_loss = losses.mse(data, recon)
-            kld_loss = -.05 * tf.reduce_sum((1 + logs2 - tf.square(mu) - tf.exp(logs2)), axis=-1)
+            recon_loss = losses.mean_squared_error(data, recon)
+            kld_loss = tf.reduce_mean(-0.5 * tf.reduce_sum((1 + logs2 - tf.square(mu) - tf.exp(logs2)), axis=1))
 
-            total_loss = recon_loss + 0.1 * kld_loss
+            print(0 if self.era == 0 else 1)            
+            total_loss = recon_loss + (0 if self.era == 0 else 1) * kld_loss
 
             grads = tape.gradient(total_loss, self.trainable_variables)
             self.optimizer.apply_gradients(zip(grads, self.trainable_variables))
@@ -95,8 +94,8 @@ class VAE(models.Model):
 
             return {
                 "loss": self.total_loss_tracker.result(),
-                "recon_loss": self.recon_loss_tracker.result(),
-                "kld_loss": self.kld_loss_tracker.result()
+                "recon": self.recon_loss_tracker.result(),
+                "kld": self.kld_loss_tracker.result()
             }
 
 
@@ -107,11 +106,11 @@ if not load_saved_model:
     opt = optimizers.Adam()
     loss = losses.MeanSquaredError()
 
-    batchsize = 64
-    vae = VAE(512)
+    batchsize = 32
+    vae = VAE(64)
     vae.compile(optimizer=optimizers.Adam())
 
-    for i in range(10):
+    for i in range(20):
         stream = []
         env.reset()
 
@@ -119,13 +118,16 @@ if not load_saved_model:
             action = env.action_space.sample()
             obs, reward, terminated, truncated, info = env.step(action)
             if terminated or truncated:
-                obs = env.reset()[0]
+                env.reset()
+
+            obs = tf.convert_to_tensor(obs, dtype=tf.dtypes.float32) / 255.
             obs = tf.pad(obs, [[23, 23], [48, 48], [0, 0]])
-            obs = tf.image.resize(obs, (64, 64)) / 255.
+            obs = tf.image.resize(obs, (64, 64))
             stream.append(obs)
 
         stream = tf.convert_to_tensor(stream, dtype=tf.dtypes.float32)
-        vae.fit(stream, epochs=20, shuffle=True, batch_size=batchsize, verbose=2)
+        vae.fit(stream, epochs=10, shuffle=True, batch_size=batchsize, verbose=1)
+        vae.era += 1
         gc.collect()
                     
         # with tf.GradientTape() as tape:
@@ -139,7 +141,6 @@ if not load_saved_model:
 
         #     stream = []
         #     print(i, f"{float(l): .6f}")
-        #     i += 1
     
     built_shadow = vae.get_built_shadow()
     built_shadow.save(save_name)
